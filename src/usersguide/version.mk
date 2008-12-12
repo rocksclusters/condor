b6cb7a2b75a9d43c521319ddb47f7b3c6e35b1e8
Greg Bruno <greg.bruno@gmail.com>
2008-12-12 18:12:48 +0000

    make condor docs look like other rolls' docs

---
+++ src/usersguide/version.mk
@@ -0,0 +1,11 @@
+ROLL	= condor
+NAME    = roll-$(ROLL)-usersguide
+RELEASE = 0
+
+SUMMARY_COMPATIBLE	= $(VERSION)
+SUMMARY_MAINTAINER	= Rocks Group
+SUMMARY_ARCHITECTURE	= i386, x86_64
+
+ROLL_REQUIRES		= base kernel os1 os2 hpc
+ROLL_CONFLICTS		=
+
