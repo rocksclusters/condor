00748c4e1f5f3e462d5410886744d9de304f6254
Greg Bruno <greg.bruno@gmail.com>
2010-09-07 23:53:12 +0000

    star power for gb

--- src/command/commands/report/host/condor/interface/__init__.py
+++ src/command/commands/report/host/condor/interface/__init__.py
@@ -1,12 +1,12 @@
-#$Id: __init__.py,v 1.1 2009/12/07 19:53:09 phil Exp $
+#$Id: __init__.py,v 1.2 2010/09/07 23:53:12 bruno Exp $
 # 
 # @Copyright@
 # 
 # 				Rocks(r)
 # 		         www.rocksclusters.org
-# 		       version 5.2 (Chimichanga)
+# 		         version 5.4 (Maverick)
 # 
-# Copyright (c) 2000 - 2009 The Regents of the University of California.
+# Copyright (c) 2000 - 2010 The Regents of the University of California.
 # All rights reserved.	
 # 
 # Redistribution and use in source and binary forms, with or without
@@ -54,6 +54,9 @@
 # @Copyright@
 #
 # $Log: __init__.py,v $
+# Revision 1.2  2010/09/07 23:53:12  bruno
+# star power for gb
+#
 # Revision 1.1  2009/12/07 19:53:09  phil
 # Add a command to easily print the ip (interface in condor speak) of
 # a network.
--- src/rocks/rocks-condor.csh
+++ src/rocks/rocks-condor.csh
@@ -1,4 +1,4 @@
-# $Id: rocks-condor.csh,v 1.10 2009/05/01 19:07:14 mjk Exp $
+# $Id: rocks-condor.csh,v 1.11 2010/09/07 23:53:13 bruno Exp $
 #
 # Using Condor on a Rocks cluster
 #
@@ -6,9 +6,9 @@
 # 
 # 				Rocks(r)
 # 		         www.rocksclusters.org
-# 		       version 5.2 (Chimichanga)
+# 		         version 5.4 (Maverick)
 # 
-# Copyright (c) 2000 - 2009 The Regents of the University of California.
+# Copyright (c) 2000 - 2010 The Regents of the University of California.
 # All rights reserved.	
 # 
 # Redistribution and use in source and binary forms, with or without
@@ -56,6 +56,9 @@
 # @Copyright@
 #
 # $Log: rocks-condor.csh,v $
+# Revision 1.11  2010/09/07 23:53:13  bruno
+# star power for gb
+#
 # Revision 1.10  2009/05/01 19:07:14  mjk
 # chimi con queso
 #
